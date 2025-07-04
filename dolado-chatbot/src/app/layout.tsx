fdf3e2e3bcf9ebc9e8812db21238ce9080ed7caf
Dynylson Júnior <dynylsonjr@gmail.com>
2025-07-03 23:50:59 -0300

    feat: add inter font

--- dolado-chatbot/src/app/layout.tsx
+++ dolado-chatbot/src/app/layout.tsx
@@ -1,9 +1,17 @@
+import { Inter } from 'next/font/google'
 import type { Metadata } from "next";
 import "./globals.css";
 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+  weight: ['400', '500', '600', '700', '800']
+})
+
 export const metadata: Metadata = {
-  title: "Dolado Chatbot",
-  description: "Assistente virtual de consultoria da Dolado",
+  title: "Onboarding Dolado",
+  description: "Teste prático para Frontend",
 };
 
 export default function RootLayout({
@@ -12,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="pt-BR" className={inter.variable}>
+      <body className={`bg-zinc-950 text-white ${inter.className}`}>
         {children}
       </body>
     </html>
