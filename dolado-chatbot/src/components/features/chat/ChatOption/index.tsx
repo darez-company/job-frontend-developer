45a21d6ae639a71b323647c885517762100b041b
Dynylson Júnior <dynylsonjr@gmail.com>
2025-07-05 18:02:57 -0300

    feat: add user-chosen option message to global state

--- dolado-chatbot/src/components/features/chat/ChatOption/index.tsx
+++ dolado-chatbot/src/components/features/chat/ChatOption/index.tsx
@@ -1,18 +1,35 @@
 import { Button } from "@/components/ui/button"
+import { useChatStore } from "@/store/chat-store";
+import { v4 as uuidv4 } from "uuid";
 
 interface ChatOptionProps {
     text: string;
+    handleChatOption: (show: boolean) => void;
 }
 
-export const ChatOption = ({ text }: ChatOptionProps) => {
+export const ChatOption = ({ text, handleChatOption }: ChatOptionProps) => {
+    const setMessages = useChatStore((state) => state.setMessages);
+    const botReply = useChatStore((state) => state.botReply);
+
+    function handleClick() {
+        handleChatOption(false);
+        setMessages({
+            id: uuidv4(),
+            text,
+            sender: 'user',
+        });
+        botReply();
+    }
+
     return (
         <Button
+            onClick={handleClick}
             variant="outline"
             size="sm"
-            className="flex-1 min-w-[200px] p-6 break-words whitespace-normal border-zinc-700
+            className='flex-1 min-w-[200px] p-6 break-words whitespace-normal border-zinc-700
                 transition-all duration-200 ease-in-out bg-transparent text-white hover:bg-zinc-800
                 hover:border-primary hover:text-primary-foreground cursor-pointer
-            "
+            '
         >
             {text}
         </Button>
