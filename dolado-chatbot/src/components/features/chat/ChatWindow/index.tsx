3fdfbc11b2b5e055323e9beb48c98d4c3ddab253
Dynylson Júnior <dynylsonjr@gmail.com>
2025-07-04 23:36:55 -0300

    style: make ChatWindow component responsive

--- dolado-chatbot/src/components/features/chat/ChatWindow/index.tsx
+++ dolado-chatbot/src/components/features/chat/ChatWindow/index.tsx
@@ -1,6 +1,6 @@
 export const ChatWindow = () => {
     return (
-        <div className="bg-zinc-900 h-[650px] w-[650px] rounded-2xl">
+        <div className="bg-zinc-900 h-full w-full rounded-none sm:h-[650px] sm:w-[650px] sm:rounded-2xl">
         </div>
     )
 }
