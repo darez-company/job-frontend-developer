ff4088248c0b92047df4a57efc4165c8a4640781
Dynylson Júnior <dynylsonjr@gmail.com>
2025-07-05 18:23:52 -0300

    feat: create useChatStore hook for global management

---
+++ dolado-chatbot/src/store/chat-store.ts
@@ -0,0 +1,69 @@
+import { Message } from "@/types";
+import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
+import { conversationSteps } from "@/data/conversation-steps";
+
+interface ChatStore {
+    messages: Message[];
+    setMessages: (message: Message) => void;
+    currentStepIndex: number;
+    isInFollowUp: boolean;
+    isBotTyping: boolean;
+    startConversation: () => void;
+    botReply: () => void;
+}
+
+export const useChatStore = create<ChatStore>((set, get) => ({
+    messages: [],
+    setMessages: (message: Message) => set((state) => ({
+        messages: [...state.messages, message]
+    })),
+    currentStepIndex: 0,
+    isInFollowUp: false,
+    isBotTyping: false,
+    startConversation: () => {
+        set({ isBotTyping: true });
+
+        const currentStep = conversationSteps[0];
+
+        setTimeout(() => {
+            set({ 
+                messages: [
+                    {
+                        id: uuidv4(),
+                        text: currentStep.message,
+                        sender: 'bot',
+                        options: currentStep.options || [],
+                    }
+                ],
+                isBotTyping: false,
+                currentStepIndex: 1,
+            });
+        }, 2000);
+    },
+    botReply: () => {
+        set({ isBotTyping: true });
+
+        setTimeout(() => {
+            const { currentStepIndex, messages } = get();
+
+            const currentStep = conversationSteps[currentStepIndex];
+
+            if (!currentStep) return;
+
+            set({ 
+                messages: [
+                    ...messages,
+                    {
+                        id: uuidv4(),
+                        text: currentStep.message,
+                        sender: 'bot',
+                        options: currentStep.options || [],
+                    }
+                ],
+                isBotTyping: false,
+                currentStepIndex: currentStepIndex + 1,
+            });
+        }, 1500);
+    }
+}));
