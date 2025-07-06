ef6e8bf90e2e69ed7223f1df813d6d7160003693
Dynylson Júnior <dynylsonjr@gmail.com>
2025-07-06 09:50:16 -0300

    test: add unit tests for ChatHeader component

---
+++ dolado-chatbot/src/components/features/chat/ChatHeader/index.spec.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import { ChatHeader } from '.'
+
+describe('ChatHeader', () => {
+  it('should render chatbot name (Sofia)', () => {
+    // Arrange & Act
+    render(<ChatHeader />);
+
+    // Assert
+    expect(screen.getByText('Sofia')).toBeInTheDocument();
+  });
+
+  it('should render chatbot status (Online)', () => {
+    // Arrange & Act
+    render(<ChatHeader />);
+
+    // Assert
+    expect(screen.getByText('Online')).toBeInTheDocument();
+  });
+
+  it('should render chatbot icon', () => {
+    // Arrange & Act
+    render(<ChatHeader />);
+
+    const icon = screen.getByLabelText('Bot Icon');
+
+    // Assert
+    expect(icon).toBeInTheDocument();
+  });
+})
