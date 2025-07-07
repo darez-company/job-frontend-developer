f42da4fe4df2e3a90ad7728fd7a8d17168f8fc79
Dynylson Júnior <dynylsonjr@gmail.com>
2025-07-06 23:06:14 -0300

    feat: create ExportChat component to export conversation as .txt file

---
+++ dolado-chatbot/src/components/features/chat/ChatMenu/components/export-chat.tsx
@@ -0,0 +1,39 @@
+import { MenubarItem } from "@/components/ui/menubar";
+import { useChatStore } from "@/store/chat-store";
+import { Message } from "@/types";
+
+function formatConversationAsTxt(messages: Message[]): string {
+    return messages.map(message => {
+        const sender = message.sender === 'bot' ? 'Sofia:' : 'Você:';
+
+        return `${sender}\n${message.text}`;
+    }).join('\n\n\n')
+};
+
+export const ExportChat = () => {
+    const messages = useChatStore((state) => state.messages);
+
+    function handleExport() {
+        if (messages.length === 0) return;
+
+        const formattedText = formatConversationAsTxt(messages);
+        const blob = new Blob([formattedText], { type: 'text/plain;charset=utf-8;' }); 
+        const link = document.createElement("a");
+        const url = URL.createObjectURL(blob);
+
+        link.setAttribute("href", url);
+        link.setAttribute("download", "conversa-dolado.txt"); 
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        
+        link.click();
+
+        document.body.removeChild(link);
+    };
+
+    return (
+        <MenubarItem onClick={handleExport} className="cursor-pointer">
+            Exportar conversa
+        </MenubarItem>
+    )
+}
--- dolado-chatbot/src/components/features/chat/ChatMenu/index.tsx
+++ dolado-chatbot/src/components/features/chat/ChatMenu/index.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/menubar"
 import { useChatStore } from "@/store/chat-store";
 import { Settings } from "lucide-react"
+import { ExportChat } from "./components/export-chat";
 
 export const ChatMenu = () => {
     const resetConversation = useChatStore((state) => state.resetConversation);
@@ -15,15 +16,13 @@ export const ChatMenu = () => {
         <Menubar className="bg-transparent border-none shadow-none">
         <MenubarMenu>
             <MenubarTrigger className="text-zinc-400 hover:text-white transition-colors p-2 cursor-pointer">
-            <Settings size={24} />
+                <Settings size={24} />
             </MenubarTrigger>
             <MenubarContent className="bg-zinc-900 text-white border-zinc-700 w-56">
-            <MenubarItem onClick={resetConversation} className="cursor-pointer">
-                Nova conversa
-            </MenubarItem>
-            <MenubarItem className="cursor-pointer">
-                Exportar conversa
-            </MenubarItem>
+                <MenubarItem onClick={resetConversation} className="cursor-pointer">
+                    Nova conversa
+                </MenubarItem>
+                <ExportChat />
             </MenubarContent>
         </MenubarMenu>
         </Menubar>
